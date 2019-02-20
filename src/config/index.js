9e60737559dc3b42df1b0bf445a97140660e2c3e
yayanheeh <tokoxp58@gmail.com>
2019-02-20 14:07:54 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-const YOUTUBE_API_KEY = "YOUR_API_KEY";
+const YOUTUBE_API_KEY = "AIzaSyCxKOpRRefT5WNHO09-Nv-KX2R-bQ11Yr4";
 export default {
   API_URL: "http://52.232.85.160/api?vid=",
-  SEARCH_API_URL: `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&type=video&part=snippet&maxResults=40&q=`
+  SEARCH_API_URL: `https://www.googleapis.com/youtube/v3/search?key=${AIzaSyCxKOpRRefT5WNHO09-Nv-KX2R-bQ11Yr4}&type=video&part=snippet&maxResults=40&q=`
 }
